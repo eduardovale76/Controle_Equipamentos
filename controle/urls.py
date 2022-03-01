45fd76a1f1634d1df664ed380e9272534dc419e8
Eduardo Vale <eduardovale76@gmail.com>
2022-03-01 20:31:00 -0300

    Aula #2

--- controle/urls.py
+++ controle/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('equipamento/', include('equipamento.urls') )
+    path('equipamento/', include('equipamento.urls')),
+    path('auth/', include('usuarios.urls'))
 ]
--- equipamento/views.py
+++ equipamento/views.py
@@ -1,5 +1,11 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from usuarios.views import Usuario
 
-def cadastrar(request):
-    return HttpResponse('Hello')
+
+def home(request):
+    if request.session.get('usuario'):
+        usuario = Usuario.objects.get(id = request.session['usuario']).nome
+        return HttpResponse(f'Ola, {usuario}')
+    else:
+        return redirect('/auth/login/?status=2')
